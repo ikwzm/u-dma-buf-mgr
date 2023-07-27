677f78ea0850783cc31738b6351b3720964db797
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-07-27 09:00:21 +0900

    [update] DRIVER_VERSION to 4.5.0 because the DRIVER_VERSION of u-dma-buf was updated 4.5.0

--- u-dma-buf-mgr.c
+++ u-dma-buf-mgr.c
@@ -51,7 +51,7 @@ MODULE_DESCRIPTION("U-dma-buf(User space mappable DMA buffer device driver) Mana
 MODULE_AUTHOR("ikwzm");
 MODULE_LICENSE("Dual BSD/GPL");
 
-#define DRIVER_VERSION     "4.4.4"
+#define DRIVER_VERSION     "4.5.0"
 #define DRIVER_NAME        "u-dma-buf-mgr"
 
 /**
